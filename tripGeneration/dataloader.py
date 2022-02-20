fab9f7109bd4da07b6c4a78f3ed63a703a7f466a
zhiyan_yi <u1207314@utah.edu>
2022-02-20 15:45:54 -0700

    initialize package and add dataloader.py file

---
+++ tripGeneration/dataloader.py
@@ -0,0 +1,37 @@
+"""
+Load required data for trip generation.
+All required data should be placed in the same folder.
+The required data are listed as follows:
+    1. stochastic daily activity data created by Markov Chain (csv format)
+    2. household location for each driver (no. of activities == no. household locations) (csv format)
+    3. TAZ data in the study area (shp format)
+    4. POI data in the study area (csv)
+    5. OD folder with OD distribution data in different time period (csv format)
+    6. OSM network (by inputting the bounding box txt file (txt format)
+"""
+
+import logging
+
+
+def load_stochastic_activity_data():
+    pass
+
+
+def load_household_location_data():
+    pass
+
+
+def load_taz_data():
+    pass
+
+
+def load_poi_data():
+    pass
+
+
+def load_od_data():
+    pass
+
+
+def load_osm_network():
+    pass

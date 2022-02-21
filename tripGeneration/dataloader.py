99e49f74b11f2b49fb4b0ecd6e08f13496cb5d1b
zhiyan <u1207314@utah.edu>
2022-02-21 00:34:30 -0700

    implementing dataloader functions

--- tripGeneration/dataloader.py
+++ tripGeneration/dataloader.py
@@ -18,8 +18,11 @@ from pathlib import Path
 
 import geopandas as gpd
 import networkx
+import osmnx as ox
 import pandas as pd
 from shapely import wkt
+
+logging.basicConfig(level=logging.INFO)
 
 
 def load_stochastic_activity_data(activity_filepath: pathlib.WindowsPath) -> pd.DataFrame:
@@ -37,6 +40,7 @@ def load_stochastic_activity_data(activity_filepath: pathlib.WindowsPath) -> pd.
         return the activity dataframe
     """
     activity_df = pd.read_csv(activity_filepath)
+    logging.info("========activity data is successfully loaded.========")
     return activity_df
 
 
@@ -63,6 +67,7 @@ def load_taz_data(taz_filepath: pathlib.WindowsPath) -> gpd.GeoDataFrame:
     taz_df['centroid'] = taz_df['centroid'].apply(wkt.loads)
     taz_df['geometry'] = taz_df['geometry'].apply(wkt.loads)
     taz_gdf = gpd.GeoDataFrame(taz_df, geometry = "geometry")
+    logging.info("========TAZ data is successfully loaded.=========")
     return taz_gdf
 
 
@@ -81,15 +86,28 @@ def load_poi_data(poi_filepath: pathlib.WindowsPath) -> pd.DataFrame:
         return the POI dataframe
     """
     poi_df = pd.read_csv(poi_filepath)
-    poi_df['geometry'] = poi_df['geometry'].apply(wtk.loads)
+    poi_df['geometry'] = poi_df['geometry'].apply(wkt.loads)
+    logging.info("========POI data is successfully loaded.=========")
     return poi_df
 
 
-def load_od_data():
-    pass
+def load_od_data(od_folder_path):
+    """
+    The od_folder must include 4 OD csv files: od_1.csv, od_2.csv, od_3.csv, od_4.csv
+
+    :param od_folder_path:
+    :return:
+    """
+    od_files = [f for f in od_folder_path.iterdir() if f.suffix == ".csv"]
+    od_dict = dict()
+    for od_file in od_files:
+        od_dict[od_file.name] = pd.read_csv(od_file)
+    logging.info("========OD data is successfully loaded.=========")
+    return od_dict
 
 
-def load_osm_network(north: float, south: float, east: float, west: float) -> networkx.MultiDiGraph:
+def load_osm_network(north: float, south: float, east: float, west: float, crs: str = "epsg:26912")\
+        -> networkx.MultiDiGraph:
     """
     This function resorts to osmnx package to fetch drivable road network in the bounding box.
     Missing speed and travel time are computed.
@@ -104,24 +122,27 @@ def load_osm_network(north: float, south: float, east: float, west: float) -> ne
         eastern lon of bounding box
     west: float
         western lon of bounding box
-
+    crs: string
+        the projected coordinate system
     Returns
     -----------------
     proj_graph: networkx.MultiDiGraph
         the multi directional graph representing the road network in OpenStreetMap.
     """
-    graph = ox.graph_from_bbox(north, south, east, west, network_type = "drive")
-    proj_graph = ox.prject_graph(graph, to_crs = crs)
+    graph = ox.graph_from_bbox(north, south, east, west, network_type="drive")
+    proj_graph = ox.project_graph(graph, to_crs=crs)
     proj_graph = ox.add_edge_speeds(proj_graph)
     proj_graph = ox.add_edge_travel_times(proj_graph)
+    logging.info("========Network graph is successfully fetched from OSM.=========")
     return proj_graph
 
 
 def load_required_dataset(folder_path, **kwargs):
+    logging.info("********start to load datasets********")
     if "taz" in kwargs:
         taz_filename = kwargs["taz"]
     else:
-        taz_filename = "taz.csv"
+        taz_filename = "taz.shp"
 
     if "network_bbox" in kwargs:
         network_filename = kwargs["network_bbox"]
@@ -138,9 +159,47 @@ def load_required_dataset(folder_path, **kwargs):
     else:
         poi_filename = "poi.csv"
 
+    if "od" in kwargs:
+        od_folder = kwargs["od"]
+    else:
+        od_folder = "od_folder"
+
     # check if those files exist
+    folder_dir = Path(folder_path)
+    od_dir = folder_dir.joinpath(od_folder)
+    taz_fp = folder_dir.joinpath(taz_filename)
+    poi_fp = folder_dir.joinpath(poi_filename)
+    network_fp = folder_dir.joinpath(network_filename)
+    activity_fp = folder_dir.joinpath(activity_filename)
+    # add hh distribution later
+    if not folder_dir.is_dir():
+        raise FileNotFoundError("The input file directory does not exist.")
+    if not od_dir.is_dir():
+        raise FileNotFoundError("The OD folder does not exist.")
+    if not taz_fp.is_file():
+        raise FileNotFoundError("The taz file does not exist.")
+    if not poi_fp.is_file():
+        raise FileNotFoundError("The POI file does not exist.")
+    if not network_fp.is_file():
+        raise FileNotFoundError("The network file does not exist.")
+    if not activity_fp.is_file():
+        raise FileNotFoundError("The activity file does not exist.")
+    # add check for hh distribution later
 
     # start to load each data
-
+    activity_df = load_stochastic_activity_data(activity_fp)
+    poi_df = load_poi_data(poi_fp)
+    taz_gdf = load_taz_data(taz_fp)
+    od_dict = load_od_data(od_dir)
+    with open(network_fp) as f:
+        bbox_string_list = f.read().split(",")
+        north, south, east, west = list(map(float, bbox_string_list))
+        network_graph = load_osm_network(north, south, east, west)
     # return the packed dataset in a dictionary
+    logging.info("********all required data are successfully loaded.********")
     return
+
+
+if __name__ == "__main__":
+    filepath = "/Users/zhiyan_yi/Desktop/input_data"
+    load_required_dataset(filepath)
